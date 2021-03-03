fixed perfect_reconstruction_test failing by finding bug in t computation

src/custom_layers/cs_layers.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from src.utility import get_psf,create_psf_matrix
+from src.utility import get_psf,create_psf_matrix,old_psf_matrix
 from src.custom_layers.utility_layer import downsample,upsample
 
 class CompressedSensingInception(tf.keras.layers.Layer):
@@ -96,20 +96,20 @@ class CompressedSensing(tf.keras.layers.Layer):
         super(CompressedSensing, self).__init__(*args, **kwargs, dtype="float32")
         self._iterations = tf.constant(100, dtype=tf.int32)
 
-        self._sigma = 180
+        self._sigma = 150
         self._px_size = 100
         self.matrix_update()#mat and psf defined outside innit
 
         self.mu = tf.Variable(initial_value=tf.ones((1)), dtype=tf.float32, trainable=False)
         self.lam = tf.Variable(initial_value=tf.ones((1)), dtype=tf.float32, name="lambda", trainable=True)*0.005#was0.005
-        self.t = tf.Variable(initial_value=tf.ones((1)),dtype=tf.float32, trainable=False)
         #dense = lambda x: tf.sparse.to_dense(tf.SparseTensor(x[0], x[1], tf.shape(x[2], out_type=tf.int64)))
         #self.sparse_dense = tf.keras.layers.Lambda(dense)
-        self.y = tf.Variable(initial_value=tf.zeros((5329,3)), dtype=tf.float32, trainable=False)[tf.newaxis, :]#todo: use input dim
+        self.y = tf.Variable(initial_value=tf.zeros((5184,3)), dtype=tf.float32, trainable=False)[tf.newaxis, :]#todo: use input dim
         #self.result = tf.Variable(np.zeros((73,73,3)), dtype=tf.float32, trainable=False)[tf.newaxis, :]
         self.flatten= tf.keras.layers.Flatten()
         self.tcompute = tf.keras.layers.Lambda(lambda t: (1+tf.sqrt(1+4*tf.square(t)))/2)
         self.matmul = tf.keras.layers.Lambda(lambda x: tf.keras.backend.dot(x,x))
+        #todo: add reshape
 
     @property
     def px_size(self):
@@ -144,7 +144,10 @@ class CompressedSensing(tf.keras.layers.Layer):
         self.mat = self.psf_initializer()
 
     def psf_initializer(self):
-        mat = create_psf_matrix(9, 8, self.psf)
+        #mat1 = create_psf_matrix(9, 8, self.psf)
+        mat = old_psf_matrix(9,9,72,72, 1.50*8/(81),1.50*8/(81)).T#todo: depending on sigma and px_size
+        # mat1 /= mat1[0,0]
+        # mat1 *= mat[0,0]
         return mat
 
     def softthresh2(self, input, lam):
@@ -161,14 +164,13 @@ class CompressedSensing(tf.keras.layers.Layer):
             im = self.flatten(inp[i])
             y_new_last_it = tf.zeros_like(y_n[i])
             y_tmp = y_n[i]
-            y_new = tf.zeros_like(y_tmp)
             t = tf.constant((1.0),dtype=tf.float32)
             for j in range(self.iterations):#todo iteration as variable
-                re =tf.linalg.matvec(self.mat, im- tf.linalg.matvec(tf.transpose(self.mat), y_tmp))
+                re =tf.linalg.matvec(self.mat, im - tf.linalg.matvec(tf.transpose(self.mat), y_tmp))
                 w = y_tmp+1/self.mu*re
                 y_new = self.softthresh2(w, self.lam/self.mu)#todo: not exactly the same as softthresh
                 t_n = self.tcompute(t)
-                y_tmp = y_new+ (self.t-1)/t_n*(y_new-y_new_last_it)
+                y_tmp = y_new+ (t-1)/t_n*(y_new-y_new_last_it)
                 y_new_last_it = y_new
                 t = t_n
             r.append(y_new)

src/utility.py
@@ -188,101 +188,18 @@ def create_shift_data(data_tensor, denoising, truth_array=None, th=0.1):
     return train_new, truth_new, coord_list
 
 
-def bin_localisations(data_tensor, denoising, truth_tensor=None, th=0.1):
-    train_new = []
-    truth_new = []
-    coord_list = []
-    bining = Binning()
-
-    one = denoising.predict(data_tensor[:, :, :, 0:1])
-    two = denoising.predict(data_tensor[:, :, :, 1:2])
-    three = denoising.predict(data_tensor[:, :, :, 2:3])
-    im = tf.concat([one, two, three], -1)
-    im = im/tf.keras.backend.max(im)
-
-    for i in range(im.shape[0]):
-        # todo: denoise all at once
-
-        wave = im[i:i + 1, :, :, 1:2]
-        y = tf.constant([th])
-        mask = tf.greater(wave, y)
-        wave_mask = wave * tf.cast(mask, tf.float32)
-        fig, axs = plt.subplots(3)
-        axs[0].imshow(im[i, :, :, 0])
-        axs[1].imshow(im[i, :, :, 1])
-        axs[2].imshow(im[i, :, :, 2])
-        plt.show()
-        #ax.imshow(wave[0,:,:,0])
-        coords = bining.get_coords(wave_mask.numpy()[0, :, :, 0])
-        # todo: crop PSFs done here
-        for coord in coords:
-            # ax.add_patch(rect)
-            # ax.set_title("original", fontsize=10)
-            if coord[0] - 4 > 0 and coord[1] - 4 > 0 and coord[0] + 4 < 64 and coord[1] + 4 < 64:
-                crop = data_tensor[i, coord[0] - 4:coord[0] + 5, coord[1] - 4:coord[1] + 5, :]
-
-                fig, axs = plt.subplots(3)
-                axs[0].imshow(crop[ :, :, 0])
-                axs[1].imshow(crop[ :, :, 1])
-                axs[2].imshow(crop[ :, :, 2])
-                plt.show()
-                train_new.append(crop)
-                if truth_tensor is not None:
-                    truth = truth_tensor[i:i + 1, coord[0] - 1:coord[0] + 2, coord[1] - 1:coord[1] + 2, :]
-                    ind = tf.argmax(tf.reshape(truth[0, :, :, 0], [-1]))
-                    x = ind // truth.shape[2]
-                    y = ind % truth.shape[2]
-                    x_f = tf.cast(x, tf.float64)#todo: care theres somthing wrong here
-                    y_f = tf.cast(y, tf.float64)
-                    x_f += truth[0, x, y, 1] + 4  # add difference to crop dimension
-                    y_f += truth[0, x, y, 2] + 4
-                    truth_new.append(tf.stack([x_f, y_f]))
-                coord_list.append(coord - 4)
-
-                # todo: calc coordinates why?
-    train_new = tf.stack(train_new)
-    truth_new = tf.stack(truth_new)
-    return train_new, truth_new, coord_list
 
 
-def generate_tf_dataset(image, truth):
-    gen = data_generator_coords(image, truth)
-    image = np.zeros((100, 64, 64, 3))
-    truth = np.zeros((100, 64, 64, 3))
-
-    for i in range(100):
-        image[i], truth[i], _ = gen.__next__()
-
-    image_tf1 = tf.convert_to_tensor(image[0:50, :, :])
-    image_tf2 = tf.convert_to_tensor(image[50:100, :, :])
-    truth_tf1 = tf.convert_to_tensor(truth[0:50, :, :])
-    truth_tf2 = tf.convert_to_tensor(truth[50:100, :, :])
 
 
-def simulate_where_add(input, lo_lim, up_lim, val, sparse_dense):
-    condition = tf.logical_and(input>lo_lim,input<up_lim)
-    indices_t = tf.where(condition)#[:,-1]
-    indices_f = tf.where(tf.logical_not(condition))#[:,-1]
 
-    values_remove = tf.tile([0], [tf.shape(indices_f)[0]])
-    values_remove = tf.cast(values_remove, tf.float64)
 
-    value = tf.gather_nd(input, indices_t)
-    value += val
-
-    idx_remove = indices_f#tf.stack([zeros_remove, indices_f],axis=1)
-    idx_keep = indices_t#tf.stack([zeros_keep, indices_t], axis=1)
-
-    #out =tf.SparseTensor(idx_keep, value, tf.shape(input, out_type=tf.int64))# sparse_add((tf.SparseTensor(idx_remove, values_remove, tf.shape(input, out_type=tf.int64)),
-                        #tf.SparseTensor(idx_keep, value, tf.shape(input, out_type=tf.int64))))
-    z = sparse_dense((idx_keep, value,input))
-    return z
 
 
 def get_psf(sigma, px_size):
     size = 256 # should always be enough
 
-    sigma_x = sigma/(px_size)*8
+    sigma_x = sigma/81*8#neglecting coarse and.. resolution
     sigma_y = sigma_x
     psf = np.zeros((size, size))#todo: somewhere factor 2 missing!
     psf_tf = tf.zeros((size,size))
@@ -294,8 +211,12 @@ def get_psf(sigma, px_size):
     # PI = tf.constant(np.pi)
     # psf_tf = 1  / (tf.sqrt(2.0 * PI * tf_sigma_x ** 2.0) * tf.sqrt(2.0 * PI * tf_sigma_y ** 2.0)) \
     #                     * tf.exp(-(I/ (2.0 * tf_sigma_x ** 2.0) + J / (2.0 * tf_sigma_y** 2.0)))
+
+    skaling_fine = 1/71
+    skaling_coarse = 1/8
+
     for i in range(psf.shape[0]):
-        for j in range(psf.shape[1]):
+        for j in range(psf.shape[1]):#todo: normed j is not an even number...
             normed_i = (i ) ** 2
             normed_j = (j ) ** 2
             psf[i, j] = 1  / (np.sqrt(2 * np.pi * sigma_x ** 2) * np.sqrt(2 * np.pi * sigma_y ** 2)) \
@@ -314,10 +235,63 @@ def create_psf_block(idy, size_x, psf,size_y):
     return blockA
 
 def create_psf_matrix(size_x, magnification, psf):
-    size_y = size_x*magnification+1
+    size_y = size_x*magnification
     matrix = np.zeros((size_y**2, size_x**2)).astype(np.float32)
     for i in range(size_x):
         for j in range(size_y):
             matrix[j * size_y:(j + 1) * size_y, i * size_x:(i + 1) * size_x] = create_psf_block(abs(size_x * i - j), size_x, psf, size_y)
     return matrix
 
+def old_psf_matrix(M_x,M_y,N_x,N_y,sigma_x,sigma_y):
+    m = M_x*M_y    # Anzahl der Gitterpunkte des groben Gitters
+    N = N_x*N_y
+    if sigma_x == None and sigma_y == None:
+        sigma_x = 5/N
+        sigma_y = 5/N
+        a_x=1
+        a_y=1
+    else:
+        a_x=1
+        a_y=1
+
+    h_M_x = a_x/(M_x-1)  # Gitterweite des groben Gitters in x-Richtung
+    h_M_y = a_y/(M_y-1)  # Gitterweite des groben Gitters in y-Richtung
+    h_N_x = a_x/(N_x-1)  # Gitterweite des feinen Gitters in
+    h_N_y = a_y/(N_y-1)  # x- bzw. y-Richtung
+    A = np.zeros((m,N))
+
+    # Die Punkte der Gitter werden spaltenweise als Vektor untereinandergehängt
+    # und umbenannt: grobe-Gitter-Matrix U wird zu Vektor y der Länge m,
+    # feine-Gitter-Matrix V wird zu Vektor x der Länge N
+    K = np.zeros([m,2]) # die i-te Zeile von K entspricht den ursprünglichen Zeilen-
+                        # und Spaltenindizes der i-ten Komponente von y
+    L = np.zeros([N,2]) # die i-te Zeile von L entspricht den ursprünglichen Zeilen-
+                        # und Spaltenindizes der i-ten Komponente von x
+    #x_K = [(M_y-1),-1:0]# Vektor für zweite Spalte von K
+    #x_L = [(N_y-1),-1:0]# Vektor für zweite Spalte von L
+
+    x_K = np.arange(M_y-1,-1,-1)
+    x_L = np.arange(N_y-1,-1,-1)
+
+    # Gauß-Kern
+    def gaus_kernel(x,y):
+        return h_N_x*h_N_y*1/(np.sqrt(2*np.pi*sigma_x**2)*np.sqrt(2*np.pi*sigma_y**2))*np.exp(-(x**2/(2*sigma_x**2)+y**2/(2*sigma_y**2)))
+
+
+    for i in range(M_x):
+        y_K=i*np.ones(M_y) # todo: 1+???
+        K[(i*M_y):(M_y+i*M_y),0]=y_K    # in der ersten Spalte erhöht sich der Wert
+                                           # eines Elements nach M Zeilen um 1
+        K[(i*M_y):(M_y+i*M_y),1]=x_K    # in der zweiten Spalte erscheint M-mal die
+                                           # Zahlenfolge M-1:-1:0
+    for i in range(N_x):
+        y_L=i*np.ones(N_y)
+        L[(i*N_y):(N_y+i*N_y),0]=y_L # in der ersten Spalte erhöht sich der Wert
+                                        # eines Elements nach N_y Zeilen um 1
+        L[(i*N_y):(N_y+i*N_y),1]=x_L # in der zweiten Spalte erscheint N_x-mal die
+                                        # Zahlenfolge N_y-1:-1:0
+    for i in range(m):   # i-te Zeile
+        for j in range(N):   # j-te Spalte
+            print(K[i,0]*h_M_x-L[j,0]*h_N_x)
+            A[i,j] = gaus_kernel(K[i,0]*h_M_x-L[j,0]*h_N_x,  K[i,1]*h_M_y-L[j,1]*h_N_y)
+    return A

tests/test_compressed_sensing.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from src.custom_layers.cs_layers import CompressedSensingInception, CompressedSensing
 from unittest import skip
 from tests.test import BaseTest
+import matplotlib.pyplot as plt
 
 #done: unit test should extend tf test case
 class TestCompressedSensingLayer(BaseTest):
@@ -32,9 +33,13 @@ class TestCompressedSensingLayer(BaseTest):
         output2 = self.layer(data)
         self.assertNotAllClose(output1, output2, msg="Different iterations yiel the same output...")
 
-    @skip
     def test_perfect_reconstruction_from_noiseless_data(self):
-        self.fail()
+        #todo: simulate perfect psf use cs high iteration receive sparse result
+        data = self.create_noiseless_random_data_crop(9, sigma=150, px_size=100)
+        self.layer.iterations = 4000#todo: 4000 is close to sparse
+        output = self.layer(data)
+        output = tf.reshape(output, (1,72,72,3))
+        self.assertGreater(tf.where(output>0).shape[0], 50)
 
     @skip
     def test_psf_matrix(self):