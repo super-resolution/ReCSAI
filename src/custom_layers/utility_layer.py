27d368718603565e5ce474c666e4cb1739a47885
biophys <sebastian.reinhard@stud-mail.uni-wuerzburg.de>
2022-03-22 16:08:13 +0100

    removed deprecated

--- src/custom_layers/utility_layer.py
+++ src/custom_layers/utility_layer.py
@@ -1,33 +1,6 @@
 import tensorflow as tf
 
 
-class Shifting(tf.keras.layers.Layer):
-    def __init__(self):
-        def layer(inp):
-            return tfa.image.translate(inp[0],inp[1],"BILINEAR")
-        self.shift_layer = tf.keras.layers.Lambda(layer)
-
-    def restack(self, in1, in2):
-        columns = []
-
-        for i,col in enumerate(tf.unstack(in2, axis=1)):
-            columns.extend([in1[:,i], col])
-        columns.append(in1[:,i+1])
-        return tf.stack(columns,axis=1)
-
-    def __call__(self, input,shift):
-        odd = True
-        if input[0,0,0,0] ==0:
-            odd = False
-        substack1 = input[:,::2]
-        substack2 = input[:,1::2]
-        if odd:
-            substack2 = self.shift_layer((substack2, shift))
-        else:
-            substack1 = self.shift_layer((substack1, shift))
-        substack1 = tf.cast(substack1,tf.float64)
-        substack2 = tf.cast(substack2,tf.float64)
-        return self.restack(substack1, substack2)
 
 def downsample(filters, size, apply_batchnorm=True, strides=2, activation=True):
     initializer = tf.random_normal_initializer(0., 0.02)
